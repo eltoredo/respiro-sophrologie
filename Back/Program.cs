3e72ce94bdc71cc61d17927fd6280c366186e3b3
eltoredo <tanguy.e@hotmail.fr>
2023-09-15 16:45:13 +0200

    Auth0 setup
    Full authentication setup with Auth0

--- Back/Program.cs
+++ Back/Program.cs
@@ -1,30 +1,105 @@
-using Microsoft.EntityFrameworkCore;
-using Respiro;
+using App.Authorization;
+using App.Middlewares;
+using App.Requirement;
+using App.Services;
+using dotenv.net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.ConfigureAppConfiguration((configBuilder) =>
+{
+    configBuilder.Sources.Clear();
+    DotEnv.Load();
+    configBuilder.AddEnvironmentVariables();
+});
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.AddServerHeader = false;
+});
+
 // Add services to the container.
+builder.Services.AddScoped<IMessageService, MessageService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins(
+            builder.Configuration.GetValue<string>("CLIENT_ORIGIN_URL"))
+            .WithHeaders(new string[] {
+                HeaderNames.ContentType,
+                HeaderNames.Authorization,
+            })
+            .WithMethods("GET")
+            .SetPreflightMaxAge(TimeSpan.FromSeconds(86400));
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<MyDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Host.ConfigureServices(services =>
+{
+    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            var audience =
+                  builder.Configuration.GetValue<string>("AUTH0_AUDIENCE");
+
+            options.Authority =
+                  $"https://{builder.Configuration.GetValue<string>("AUTH0_DOMAIN")}/";
+            options.Audience = audience;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true
+            };
+        });
+
+    services.AddAuthorization(options =>
+    {
+        options.AddPolicy("read:admin-messages", policy =>
+        {
+            policy.Requirements.Add(new RbacRequirement("read:admin-messages"));
+        });
+    });
+
+    services.AddSingleton<IAuthorizationHandler, RbacHandler>();
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var requiredVars =
+    new string[] {
+          "PORT",
+          "CLIENT_ORIGIN_URL",
+          "AUTH0_DOMAIN",
+          "AUTH0_AUDIENCE",
+    };
+
+foreach (var key in requiredVars)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var value = app.Configuration.GetValue<string>(key);
+
+    if (value == "" || value == null)
+    {
+        throw new Exception($"Config variable missing: {key}.");
+    }
 }
 
-app.UseHttpsRedirection();
+app.Urls.Add(
+    $"http://+:{app.Configuration.GetValue<string>("PORT")}");
 
-app.UseAuthorization();
-
+app.UseErrorHandler();
+app.UseSecureHeaders();
 app.MapControllers();
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
